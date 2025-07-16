ad3e538a4833cd0b1283651e601a93cf4f458d7f
PedroManoel22 <pedroagmiguel@gmail.com>
2025-07-16 17:17:17 -0300

    atualizando...

--- exercicios/teste.py
+++ exercicios/teste.py
@@ -1,4 +1,11 @@
-import pandas as pd
-base_dados = pd.read_csv('dados_sensores.csv')
-base_dados = base_dados.drop(['nivel', 'pressao'], axis=1)
-print(base_dados)
+  raw = r'this\t\n and that'
+
+  # this\t\n and that
+  print(raw)
+
+  multi = """It was the best of times.
+  It was the worst of times."""
+
+  # It was the best of times.
+  #   It was the worst of times.
+  print(multi)
