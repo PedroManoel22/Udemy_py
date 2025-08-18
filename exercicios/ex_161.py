5cad674a93eadd7a09faf9a61b478451477b3c30
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-08-18 11:21:02 -0300

    .

--- exercicios/ex_161.py
+++ exercicios/ex_161.py
@@ -28,34 +28,32 @@ print(novos_produtos)
 # agora com deepcopy
 
 from copy import deepcopy
+from dados import produtos
 
-produtos = [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
-]
-
-
-novos_produtos = deepcopy(produtos)
-
-for produto in novos_produtos:
-    produto['preco'] += produto['preco'] * 0.10
-
-print("Lista original (sem alteração):")
-print(produtos)
-
-print("\nLista de novos produtos (com preços aumentados):")
-print(novos_produtos)
+novos_produtos = [
+    {**p, 'preco': round(p['preco'] * 1.1, 2)} for p in deepcopy(produtos) # não é necessário este deepcopy
+    ]
+print('Lista original:')
+print(*produtos, sep='\n')
+print()
+print('Preços com 10% de aumento:')
+print(*novos_produtos, sep='\n')
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
-produtos_ordenados_por_nome = deepcopy(novos_produtos)
-for k, v in enumerate(produtos_ordenados_por_nome):
-    print(k, v)
+print()
+print('Produtos ordenados por nome, (do maior para o menor):')
+produtos_ordenados_por_nome = sorted(deepcopy(produtos), key= lambda p : p['nome'], reverse=True)
+print(*produtos_ordenados_por_nome, sep='\n')
 
-print('Produtos ordenados por nome:', produtos_ordenados_por_nome)
+
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+print()
+print('Produtos ordenados por nome, (do menor para o maior):')
+produtos_ordenados_por_nome = sorted(deepcopy(produtos), key= lambda p : p['nome'])
+print(*produtos_ordenados_por_nome, sep='\n')
+
+
