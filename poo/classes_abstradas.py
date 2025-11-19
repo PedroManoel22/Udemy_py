a77f9c1345fb4644ae1058ab0ab197b5a54ffa92
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-11-19 12:00:08 -0300

    .

--- poo/classes_abstradas.py
+++ poo/classes_abstradas.py
@@ -14,3 +14,31 @@
 # É possível criar @property @setter @classmethod
 # @staticmethod e @method como abstratos, para isso
 # use @abstractmethod como decorator mais interno.
+# para uma classe ser abstrada, esta classe tem que herdar ABC e ter pelo menos um método abstrado
+# uma classe abstrada não deve ser instanciada
+
+from abc import ABC, ABCMeta, abstractmethod
+
+#class log(metaclass=ABCMeta): 
+
+class Log(ABC):
+    @abstractmethod
+    def _log(self, msg):...
+    
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+
+    
+    def log_sucess(self, msg):
+        return self._log(f'Sucess: {msg}')
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+l = LogPrintMixin()
+l.log_error('oi')
