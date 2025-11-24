acb7f14d23b5e449a607314fa4fb2bc905c490df
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-11-24 14:56:56 -0300

    .

---
+++ polimorfismo.py
@@ -0,0 +1,42 @@
+# Polimorfismo -> é o princípio que permite que classes derivadas de uma mesma
+# superclasse tenham métodos iguais (com mesma assinatura) mas com comportamentos diferentes
+
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    @abstractmethod
+    def enviar(self) -> bool:
+        ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self):
+        print('Email: enviando - ', self.msg)
+        return True
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self):
+        print('SMS: enviando - ', self.msg)
+        return False
+
+
+# n = NotificacaoEmail('Testando')
+# n.enviar()
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada =   notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada')
+    
+    else:
+        print('Notificação não enviada')
+
+notificacao_email = NotificacaoEmail('Testando email')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('Testando sms')
+notificar(notificacao_sms)
+        
