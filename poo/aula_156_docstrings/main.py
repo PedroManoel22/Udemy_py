9bd02c98273beb52541b9e356fa8ad59f460ddd2
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-01-07 18:51:43 -0300

    .

---
+++ poo/aula_156_docstrings/main.py
@@ -0,0 +1,7 @@
+# import uma_linha
+# import varias_linhas
+import documentando_funcoes
+
+# print(dir(uma_linha))
+# help(varias_linhas)
+help(documentando_funcoes)
---
+++ poo/aula_156_docstrings/uma_linha.py
@@ -0,0 +1,5 @@
+
+"""O que seu módulo faz"""
+variavel = 10
+def funcao():
+    return 1
