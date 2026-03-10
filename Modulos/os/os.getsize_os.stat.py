40f0569648b03ecb759c75feb22b41a8f523231a
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-10 16:56:24 -0300

    .

--- Modulos/os/os.getsize_os.stat.py
+++ Modulos/os/os.getsize_os.stat.py
@@ -1,8 +1,14 @@
 # os.path.getsize e os.stat para dados dos arquivos
+
 import math
+import os
+from itertools import count
+
+caminho = os.path.join("C:", "\\Users", "Pedro", "Desktop", "exemplo")
+counter = count()
 
 
-def formata_tamanho(tamanho_em_bytes: int, base: int = 1024) -> str:
+def formata_tamanho(tamanho_em_bytes: int, base: int = 1024):
     """Formata um tamanho, de bytes para o tamanho apropriado"""
 
     # Original:
@@ -15,22 +21,61 @@ def formata_tamanho(tamanho_em_bytes: int, base: int = 1024) -> str:
     # Tupla com os tamanhos
     #                      0    1     2     3     4     5
     abreviacao_tamanhos = "B", "KB", "MB", "GB", "TB", "PB"
+
     # Logaritmo -> https://brasilescola.uol.com.br/matematica/logaritmo.htm
     # math.log vai retornar o logaritmo do tamanho_em_bytes
     # com a base (1000 por padrão), isso deve bater
     # com o nosso índice na abreviação dos tamanhos
+
     indice_abreviacao_tamanhos = int(math.log(tamanho_em_bytes, base))
+
     # Por quanto nosso tamanho deve ser dividido para
     # gerar o tamanho correto.
+
     potencia = base**indice_abreviacao_tamanhos
+
     # Nosso tamanho final
+
     tamanho_final = tamanho_em_bytes / potencia
+
     # A abreviação que queremos
+
     abreviacao_tamanho = abreviacao_tamanhos[indice_abreviacao_tamanhos]
 
     return f"{tamanho_final:.2f} {abreviacao_tamanho}"
 
 
-print(formata_tamanho(1000))
-print(formata_tamanho(1000000))
-print(formata_tamanho(1000000000))
+print()
+
+print()
+
+if __name__ == "__main__":
+    print(formata_tamanho(1_000))
+    print(formata_tamanho(1_000_000))
+    print(formata_tamanho(1_000_000_000))
+
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+
+    # Pasta atual
+    print(the_counter, "pasta_atual: ", root)
+
+    # subdiretórios
+    for dir_ in dirs:
+        print("  ", the_counter, "Dir: ", dir_)
+
+    # arquivos do diretório atual
+    for file in files:
+        caminho_completo_arquivo = os.path.join(
+            root, file
+        )  # caminho completo do arquivo
+        # tamanho_arquivo = os.path.getsize(caminho_completo_arquivo) # pegando o tamanho do arquivo
+        stats = os.stat(
+            caminho_completo_arquivo
+        )  # aqui também pegamos o tamanho do arquivo
+        tamanho = stats.st_size
+        print("  ", the_counter, "File: ", file, formata_tamanho(tamanho))
+
+        # print("    ", the_counter, "File: ", caminho_completo_arquivo)
+        # os.unlink(caminho_completo_arquivo)  # tome cuidado, pois aqui vai excluir todos os arquivos, enão tem lixeira
---
+++ Modulos/os/os.shutil(copiar_pastas_p_outra).py
@@ -0,0 +1,33 @@
+# os + shutil - Copiando arquivos com Python
+# 1 - Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+import os
+import shutil
+
+HOME = os.path.expanduser("~")  # pegando a home do usuário
+DESKTOP = os.path.join(HOME, "Desktop")
+PASTA_ORIGINAL = os.path.join(DESKTOP, "exemplo")
+NOVA_PASTA = os.path.join(DESKTOP, "Nova_pasta")
+print(NOVA_PASTA)
+
+os.makedirs(
+    NOVA_PASTA, exist_ok=True
+)  # cria a pasta, exist_ok=True -> só cria a pasta se ela não existir
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        caminho_novo_diretorio = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+
+        print(caminho_novo_diretorio)
+        os.makedirs(
+            caminho_novo_diretorio, exist_ok=True
+        )  # cria a pasta, exist_ok=True -> só cria a pasta se ela não existir
+
+    for file in files:
+        caminho_arquivo = os.path.join(root, file)
+        caminho_novo_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_arquivo, caminho_novo_arquivo)
