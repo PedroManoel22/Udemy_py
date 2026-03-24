392fe0c20ff0ce4f75ddbcf9b25d7465ba7539fa
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-24 15:03:43 -0300

    Create Modulos\enviando_email.py

---
+++ Modulos/eviando_email.py
@@ -0,0 +1,47 @@
+# enviando E-mails SMTP com Python
+import os
+import pathlib
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from string import Template
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Caminho arquivo HTML
+CAMINHO_HTML = pathlib.Path(__file__).parent / "email.html"
+
+# Dados do remetente e destinatário
+remetente = os.getenv("FROM_EMAIL", "")
+destinatario = remetente
+
+# Configurações SMTP
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+smtp_username = os.getenv("FROM_EMAIL", "")
+smtp_password = os.getenv("EMAIL_PASSWORD", "")
+
+# Mensagem de texto
+with open(CAMINHO_HTML, "r", encoding="utf-8") as arquivo:
+    texto_arquivo = arquivo.read()
+    template = Template(texto_arquivo)
+    texto_email = template.substitute(nome="João")
+
+# Transformar nossa mensagem em MIMEMultipart
+mime_multipart = MIMEMultipart()
+mime_multipart["from"] = remetente  # quem está mandando o e-mail
+mime_multipart["to"] = destinatario  # quem está recebendo o e-mail
+mime_multipart["subject"] = "Este é o assunto do e-mail"
+
+corpo_email = MIMEText(texto_email, "html", "UTF-8")
+mime_multipart.attach(corpo_email)
+
+# Envia o e-mail
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.ehlo()
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.send_message(mime_multipart)
+    print("E-mail enviado com sucesso!")
