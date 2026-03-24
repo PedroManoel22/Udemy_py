116a8f7c3ac2f282bbb8dd2de4bf1e6eb7a9b685
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-24 09:12:52 -0300

    Update Modulos\locale\exemplo.py

--- Modulos/locale/exemplo.py
+++ Modulos/locale/exemplo.py
@@ -5,9 +5,8 @@
 import calendar
 import locale
 
-locale.setlocale(
-    locale.LC_ALL, "pt_BR.UTF-8"
-)  # mudamos o locale para o padrão do sistema operacional
+locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
+# mudamos o locale para o padrão do sistema operacional
 print(calendar.calendar(2002))
 
 print(locale.getlocale())  # como saber o locale da máquina
