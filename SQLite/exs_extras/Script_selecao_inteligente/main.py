fb992d0929cc76a0379fbf091332353a2da8c2b3
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-16 16:15:17 -0300

    Update main.py

--- SQLite/exs_extras/Script_selecao_inteligente/main.py
+++ SQLite/exs_extras/Script_selecao_inteligente/main.py
@@ -4,3 +4,27 @@
 # Tarefa: Configure o row_factory = sqlite3.Row. Faça um SELECT * na sua tabela de clientes.
 
 # Ação: Percorra o resultado com um for e imprima uma frase formatada usando as chaves: f"O cliente {row['nome']} usa o email {row['email']}".
+
+import sqlite3
+
+DB_FILE = "SQLite/exs_extras/Script_selecao_inteligente/clientes.db"
+TABLE_NAME = "clientes"
+
+
+# Conectando ao servidor
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
+
+# Selecionando toda a tabela
+
+cursor.execute(f"SELECT * FROM {TABLE_NAME}")
+
+print()
+
+for row in cursor.fetchall():
+    print(f"O cliente {row[0]} usa o email {row[1]}")
+
+print()
+
+cursor.close()
+conn.close()
