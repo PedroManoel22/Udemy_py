2dbdc8d9a655609442fbda3598776afb5d8042a7
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-16 10:56:57 -0300

    Create main.py

---
+++ SQLite/exs_extras/dicionarios_fonte_dados/main.py
@@ -0,0 +1,8 @@
+# 4. Dicionários como Fonte de Dados
+# Contexto: Em APIs modernas, os dados geralmente chegam como dicionários (JSON).
+
+# Tarefa: Crie um dicionário representando um produto: meu_produto = {'nome': 'Mouse Gamer', 'preco': 120.50}.
+
+# Ação: Insira esse dicionário na tabela usando placeholders nomeados (ex: :nome e :preco) em vez de interrogações.
+
+# Dica Pythônica: Isso torna o código muito mais legível e menos propenso a erros de ordem de colunas.
