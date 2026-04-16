f65c76929abfd26810052f9b951bf2f0edaec360
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-16 10:57:00 -0300

    Create main.py

---
+++ SQLite/exs_extras/limpeza_seguranca/main.py
@@ -0,0 +1,7 @@
+# 3. Limpeza de Segurança (DELETE e SQLite Sequence)
+# Contexto: Após realizar testes, você precisa limpar sua tabela de "Logs" para deixá-la pronta para produção, começando o ID do zero.
+
+# Tarefa: 1. delete todos os dados da tabela logs.
+# 2. Execute o comando para resetar a tabela interna sqlite_sequence (visto na aula 393).
+
+# Objetivo: Garantir que o próximo item inserido tenha o id = 1.
