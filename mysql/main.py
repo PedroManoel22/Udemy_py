bc3e9bc3b6126a69ab60a2acae2ea1af355eaf63
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-20 11:39:10 -0300

    Update main.py

--- mysql/main.py
+++ mysql/main.py
@@ -3,14 +3,43 @@
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
 
+import os
+
+import dotenv
 import pymysql
 
+TABLE_NAME = "customers"
+dotenv.load_dotenv()
+
 connection = pymysql.connect(
-    host="localhost", user="usuario", password="senha", database="banco_de_dados"
+    host=os.environ["MYSQL_HOST"],
+    user=os.environ["MYSQL_USER"],
+    password=os.environ["MYSQL_PASSWORD"],
+    database=os.environ["MYSQL_DATABASE"],
 )
+
+# print(os.environ["MYSQL_DATABASE"])
+
+# Conectando e desconectando ao servidor MySQL
 with connection:
     with connection.cursor() as cursor:
         # SQL
-        print()
-        print(cursor)
-        print()
+        cursor.execute(
+            f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(50) NOT NULL, idade INT NOT NULL)"
+        )
+
+        # cuidado: TRUNCATE TABLE apaga os dados, mas mantém a estrutura da tabela
+        cursor.execute(f"TRUNCATE TABLE {TABLE_NAME}")
+
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # SQL
+        sql = f"INSERT INTO {TABLE_NAME} (nome, idade) VALUES (%s, %s)"
+        data = (("João", 30), ("Maria", 25), ("Pedro", 35))
+
+        result = cursor.executemany(sql, data)
+
+        # print(result)  # número de linhas afetadas
+
+    connection.commit()
